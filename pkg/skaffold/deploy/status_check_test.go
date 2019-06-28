f22aff90bb1b527eb8f05491d7363b782097d5f3
Tejal Desai <tejaldesai@google.com>
2019-06-28 11:23:49 -0700

    add unit tests

--- pkg/skaffold/deploy/status_check_test.go
+++ pkg/skaffold/deploy/status_check_test.go
@@ -17,8 +17,6 @@ limitations under the License.
 package deploy
 
 import (
-	"fmt"
-	"sync"
 	"testing"
 
 	"github.com/GoogleContainerTools/skaffold/testutil"
@@ -26,7 +24,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	fakekubeclientset "k8s.io/client-go/kubernetes/fake"
-
 )
 
 func TestGetPods(t *testing.T) {
@@ -34,10 +31,11 @@ func TestGetPods(t *testing.T) {
 	var tests = []struct {
 		description string
 		pods []*v1.Pod
+		expectedPodNames map[string]bool
 		shouldErr   bool
 	}{
 		{
-			description: "multiple deployments in same namespace",
+			description: "multiple pods in same namespace",
 			pods: []*v1.Pod{
 				{
 					ObjectMeta: metav1.ObjectMeta{
@@ -59,38 +57,13 @@ func TestGetPods(t *testing.T) {
 					},
 				},
 			},
-
+			expectedPodNames: map[string]bool{"pod1": true, "pod2": true},
 		},
 		{
-			description: "multiple deployments with no progress deadline set",
-			pods: []*v1.Pod{
-				{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "pod1",
-						Namespace: "test",
-						Labels: map[string]string{
-							K8ManagedByLabelKey: labeller.skaffoldVersion(),
-						},
-					},
-				},
-				{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      "pod2",
-						Namespace: "test",
-						Labels: map[string]string{
-							K8ManagedByLabelKey: labeller.skaffoldVersion(),
-						},
-					},
-				},
-			},
-
+			description: "no pods",
 		},
 		{
-			description: "no deployments",
-
-		},
-		{
-			description: "multiple deployments in different namespaces",
+			description: "multiple pods in different namespaces",
 			pods: []*v1.Pod{
 				{
 					ObjectMeta: metav1.ObjectMeta{
@@ -111,10 +84,10 @@ func TestGetPods(t *testing.T) {
 					},
 				},
 			},
-
+			expectedPodNames: map[string]bool{"pod1": true},
 		},
 		{
-			description: "deployment in correct namespace but not deployed by skaffold",
+			description: "pod in correct namespace but not deployed by skaffold",
 			pods: []*v1.Pod{
 				{
 					ObjectMeta: metav1.ObjectMeta{
@@ -129,7 +102,7 @@ func TestGetPods(t *testing.T) {
 
 		},
 		{
-			description: "deployment in correct namespace  deployed by skaffold but previous version",
+			description: "pod in correct namespace  deployed by skaffold but previous version",
 			pods: []*v1.Pod{
 				{
 					ObjectMeta: metav1.ObjectMeta{
@@ -141,7 +114,6 @@ func TestGetPods(t *testing.T) {
 					},
 				},
 			},
-
 		},
 	}
 
@@ -152,59 +124,18 @@ func TestGetPods(t *testing.T) {
 				objs[i] = dep
 			}
 			client := fakekubeclientset.NewSimpleClientset(objs...)
-			actual, err := getPods(client.CoreV1().Pods("test"), labeller)
-			t.CheckErrorAndDeepEqual(test.shouldErr, err, nil, actual)
-		})
-	}
-}
-
-func TestGetPodStatus(t *testing.T) {
-	var tests = []struct {
-		description    string
-		deps           map[string]interface{}
-		expectedErrMsg []string
-		shouldErr      bool
-	}{
-		{
-			description: "one error",
-			deps: map[string]interface{}{
-				"pod1": "SUCCESS",
-				"pod2": fmt.Errorf("could not return within default timeout"),
-			},
-			expectedErrMsg: []string{"deployment pod2 failed due to could not return within default timeout"},
-			shouldErr:      true,
-		},
-		{
-			description: "no error",
-			deps: map[string]interface{}{
-				"pod1": "SUCCESS",
-				"pod2": "RUNNING",
-			},
-		},
-		{
-			description: "multiple errors",
-			deps: map[string]interface{}{
-				"pod1": "SUCCESS",
-				"pod2": fmt.Errorf("could not return within default timeout"),
-				"pod3": fmt.Errorf("ERROR"),
-			},
-			expectedErrMsg: []string{"deployment pod2 failed due to could not return within default timeout",
-				"deployment pod3 failed due to ERROR"},
-			shouldErr: true,
-		},
-	}
-
-	for _, test := range tests {
-		testutil.Run(t, test.description, func(t *testutil.T) {
-			syncMap := &sync.Map{}
-			for k, v := range test.deps {
-				syncMap.Store(k, v)
+			var expectedPods []v1.Pod = nil
+			if test.expectedPodNames != nil {
+				expectedPods = []v1.Pod{}
+				for _, po := range (test.pods) {
+					if _, ok := test.expectedPodNames[po.Name]; ok {
+						expectedPods = append(expectedPods, *po)
+					}
+				}
 			}
-			//err := getPodStatus(syncMap)
-			//t.CheckError(test.shouldErr, err)
-			//for _, msg := range test.expectedErrMsg {
-			//	t.CheckErrorContains(msg, err)
-			//}
+			actual, err := getPods(client.CoreV1().Pods("test"), labeller)
+			t.CheckErrorAndDeepEqual(test.shouldErr, err, expectedPods, actual)
 		})
 	}
 }
+
